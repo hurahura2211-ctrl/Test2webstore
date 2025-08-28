4a24e3d2669ce4d464d28138556a99eb71457fa9
hurahura2211-ctrl <hurahura2211@gmail.com>
2025-08-28 10:40:23 +0700

    Update page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { PRODUCTS, getProduct } from "../../lib/products";
